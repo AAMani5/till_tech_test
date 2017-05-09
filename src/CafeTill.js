58edcbe131ee1d08cef0366d16b17e55c3946132
Ashwini <ashie.oh@gmail.com>
2017-05-09 19:12:52 +0100

    Add code to be able to create Order class

--- src/CafeTill.js
+++ src/CafeTill.js
@@ -1,12 +1,13 @@
 (function(exports){
   'use strict';
 
-  function CafeTill(){
-
+  function CafeTill(cafedetails, order_class){
+    this.cafedetails = cafedetails;
+    this.order_class = order_class;
   }
 
-  CafeTill.prototype.createOrder = function () {
-
+  CafeTill.prototype.createOrder = function (tabledetails) {
+    return new this.order_class(tabledetails);
   };
 
   exports.CafeTill = CafeTill;
---
+++ src/Order.js
@@ -0,0 +1,14 @@
+(function(exports){
+  'use strict';
+
+  function Order(){
+
+  }
+
+  Order.prototype.addItem = function () {
+
+  };
+
+  exports.Order = Order;
+
+})(this);
