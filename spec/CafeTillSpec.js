22d47eef3e31ee91a5916bed0a3c8f6b2574a12d
Ashwini <ashie.oh@gmail.com>
2017-05-16 20:13:47 +0100

    Add Order class

--- spec/CafeTillSpec.js
+++ spec/CafeTillSpec.js
@@ -1,14 +1,19 @@
 describe("CafeTill", function(){
   'use strict';
-  
-  var cafetill;
+
+  var cafetill, myorder;
 
   beforeEach(function(){
-    cafetill = new CafeTill(cafedetails)
+    cafetill = new CafeTill(cafedetails, Order)
   });
 
   it("#new", function(){
     expect(cafetill instanceof CafeTill).toBe(true);
+  });
+
+  it("#createOrder", function(){
+    myorder = cafetill.createOrder({table:1, pplcount:2, pplnames:['Jane', 'John']})
+    expect(myorder instanceof Order).toBe(true);
   });
 
 });
