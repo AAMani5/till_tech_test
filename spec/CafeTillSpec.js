7687c684379b762ac4be51b2cc498e7c560e6510
Ashwini <ashie.oh@gmail.com>
2017-05-31 16:34:52 +0100

    Test drive creation of printBill method in Till class

--- spec/CafeTillSpec.js
+++ spec/CafeTillSpec.js
@@ -1,10 +1,12 @@
 describe("CafeTill", function(){
   'use strict';
 
-  var cafetill, myorder;
+  var cafetill, myorder, printer;
 
   beforeEach(function(){
-    cafetill = new CafeTill(cafedetails, Order)
+    printer = jasmine.createSpyObj('printer', ['printBill']);
+    printer.printBill.and.callFake(function() {return cafedetails['shopName'];});
+    cafetill = new CafeTill(cafedetails, Order, printer)
   });
 
   it("#new", function(){
@@ -15,5 +17,9 @@ describe("CafeTill", function(){
     myorder = cafetill.createOrder({table:1, pplcount:2, pplnames:['Jane', 'John']})
     expect(myorder instanceof Order).toBe(true);
   });
+
+  it("#printBill", function(){
+    expect(cafetill.printBill()).toEqual(cafedetails['shopName'])
+  })
 
 });
--- src/CafeTill.js
+++ src/CafeTill.js
@@ -1,17 +1,26 @@
 (function(exports){
   'use strict';
 
-  function CafeTill(cafedetails, order_class){
+  function CafeTill(cafedetails, order_class, printer){
     this.cafedetails = cafedetails;
     this.order_class = order_class;
+    this.printer = printer;
   }
 
   CafeTill.prototype.createOrder = function (tabledetails) {
     return new this.order_class(tabledetails, this._cafeDetails());
   };
 
+  CafeTill.prototype.printBill = function () {
+    return this._printer().printBill();
+  };
+
   CafeTill.prototype._cafeDetails = function () {
     return this.cafedetails;
+  };
+
+  CafeTill.prototype._printer = function () {
+    return this.printer;
   };
 
   exports.CafeTill = CafeTill;
