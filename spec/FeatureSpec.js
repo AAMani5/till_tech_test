57396f7dc7930c2094b65e98e9b1f0f86751dc60
Ashwini <ashie.oh@gmail.com>
2017-05-31 14:57:53 +0100

    Add an expectation to check for total calculation in feature test

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -1,16 +1,22 @@
 describe("Feature Tests", function(){
   'use strict';
 
-  var cafetill, janeorder;
+  var cafetill, janeorder, orderItem, totalBeforeTax, totalAfterTax, taxpercent;
 
   beforeEach(function(){
     cafetill = new CafeTill(cafedetails, Order);
+    orderItem = {quantity: 2, name: 'Cafe Latte'};
+    taxpercent = 8.64;
+    totalBeforeTax = (cafedetails['prices'][0][orderItem['name']] * orderItem['quantity']);
+    totalAfterTax = Math.round(108.64 * totalBeforeTax)/100;
   });
 
   it("can take order and print bill", function(){
     janeorder = cafetill.createOrder({table:1, pplcount:2, pplnames:['Jane', 'John']})
     expect(janeorder instanceof Order).toBe(true)
-    expect(function(){janeorder.addItem({quantity: 2, name: 'Cafe Latte'})}).not.toThrow();
+    expect(function(){janeorder.addItem(orderItem)}).not.toThrow();
+    expect(janeorder.totalBeforeTax()).toEqual(totalBeforeTax);
+    expect(janeorder.totalAfterTax()).toEqual(totalAfterTax);
   });
 
 });
