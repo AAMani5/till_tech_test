a5fe5308a5f73111a5c383b8d14f965353dc104c
Ashwini <ashie.oh@gmail.com>
2017-05-31 14:32:11 +0100

    Add a test to check the tax amount rounded to 2 decimals

--- spec/OrderSpec.js
+++ spec/OrderSpec.js
@@ -1,8 +1,9 @@
 describe("Order", function(){
   'use strict';
-  var cafetill, janeorder, firstItem, secondItem, tableDetails;
+  var cafetill, janeorder, firstItem, secondItem, tableDetails, taxpercent;
 
   beforeEach(function(){
+    taxpercent = 8.64;
     tableDetails = {table:1, pplcount:2, pplnames:['Jane', 'John']};
     firstItem = {quantity: 2, name: 'Cafe Latte'};
     secondItem = {quantity: 2, name: 'Flat White'};
@@ -28,11 +29,24 @@ describe("Order", function(){
     expect(janeorder.getItems()).toEqual([firstItem]);
   });
 
-  it("#total", function(){
+  it("#totalBeforeTax", function(){
     janeorder.addItem(firstItem);
     janeorder.addItem(secondItem);
-    var total = (cafedetails['prices'][0][firstItem['name']] * firstItem['quantity']) + (cafedetails['prices'][0][secondItem['name']] * secondItem['quantity'])
-    expect(janeorder.total()).toEqual(total);
+    var totalBeforeTax = (cafedetails['prices'][0][firstItem['name']] * firstItem['quantity']) + (cafedetails['prices'][0][secondItem['name']] * secondItem['quantity'])
+    expect(janeorder.totalBeforeTax()).toEqual(totalBeforeTax);
+  });
+
+  xit("#totalAfterTax", function(){
+    janeorder.addItem(firstItem);
+    var totalBeforeTax = cafedetails['prices'][0][firstItem['name']] * firstItem['quantity'];
+    expect(janeorder.totalAfterTax()).toEqual(1.0864 * totalBeforeTax);
+  });
+
+  it("#taxAmount", function(){
+    janeorder.addItem(firstItem);
+    var totalBeforeTax = cafedetails['prices'][0][firstItem['name']] * firstItem['quantity'];
+    var tax = Math.round(taxpercent * totalBeforeTax)/100;
+    expect(janeorder.taxAmount()).toEqual(tax);
   });
 
 });
