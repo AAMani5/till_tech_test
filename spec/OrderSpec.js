767a10620ce3e2d13f99ea5a3a1411a878223d6b
Ashwini <ashie.oh@gmail.com>
2017-05-31 11:03:57 +0100

    Test if total is calculated with two item in the order

--- spec/OrderSpec.js
+++ spec/OrderSpec.js
@@ -1,10 +1,11 @@
 describe("Order", function(){
   'use strict';
-  var cafetill, janeorder, firstItem, tableDetails;
+  var cafetill, janeorder, firstItem, secondItem, tableDetails;
 
   beforeEach(function(){
     tableDetails = {table:1, pplcount:2, pplnames:['Jane', 'John']};
     firstItem = {quantity: 2, name: 'Cafe Latte'};
+    secondItem = {quantity: 2, name: 'Flat White'};
     cafetill = jasmine.createSpyObj('cafetill', ['createOrder']);
     cafetill.createOrder.and.callFake(function() {return new Order(tableDetails, cafedetails);});
     janeorder = cafetill.createOrder(tableDetails, cafedetails);
@@ -28,7 +29,10 @@ describe("Order", function(){
   });
 
   it("#total", function(){
-    expect(janeorder.total()).toEqual(cafedetails['prices'][0][firstItem['name']] * firstItem['quantity']);
+    janeorder.addItem(firstItem);
+    janeorder.addItem(secondItem);
+    var total = (cafedetails['prices'][0][firstItem['name']] * firstItem['quantity']) + (cafedetails['prices'][0][secondItem['name']] * secondItem['quantity'])
+    expect(janeorder.total()).toEqual(total);
   });
 
 });
